8768452f58c7eec64a3d46a2af485e275670313e
Attilio <parrotchaffinch@gmail.com>
2016-07-31 12:41:43 +0200

    Use Marvin.

--- src/io/attil/decoratorj/Main.java
+++ src/io/attil/decoratorj/Main.java
@@ -1,22 +1,8 @@
 package io.attil.decoratorj;
 
-import java.io.File;
-
-import ij.ImagePlus;
-import ij.io.Opener;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Object image = new Object();
-		BaseImage baseImage = new BaseImage(image);
-		ImageFlipper imageFlipper = new ImageFlipper(baseImage);
-		BlurFilter blurFilter = new BlurFilter(imageFlipper);
-
-		Object blurredFilteredImage = blurFilter.getInternalImage();
-		
-		File file = new File("lena.gif");
-		final ImagePlus imp = new Opener().openImage( file.getAbsolutePath() );
-		imp.show();
 	}
 }
